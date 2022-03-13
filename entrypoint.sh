439f9c0edffa21d8d96cb0e0a84e283216732eef
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-13 18:37:52 +0530

    added run shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,32 +24,33 @@ fi
 
 if [ "$vkey" ]
 then
-echo "vkey: ************"
+echo "vkey: ***********"
 else
 echo "vkey:"
 fi
 
-if [ -z "$appid" ] || [ -z "$vid" ] || [ -z "$vkey" ] || [-z "$api_call_name"]
+if [ -z "$appid" ] || [ -z "$vid" ] || [ -z "$vkey" ] || [ -z "$api_call_name"]
 then
         echo "Missing required parameter. Please check that all required parameters are set"
         exit 1
 fi
 
-echo "Application is $appid.But I don't knwo the name.$api_call_name"
-# echo "#!/bin/sh -l" > runJava.sh
-# echo ""
-# echo "java -jar vosp-api-wrappers-java-<version #>.jar \\
-#         -vid \"$vid\" \\
-#         -vkey \"$vkey\" \\
-#         -action \"$api_call_name\" \\
-#         -appname \"$appid\" \\ " >> runJava.sh
+#echo "Application is $appid.But I don't knwo the name.$api_call_name"
+
+echo "#!/bin/sh -l" > runJava.sh
+echo ""
+echo "java -jar vosp-api-wrappers-java-<version #>.jar \\
+        -vid \"$vid\" \\
+        -vkey \"$vkey\" \\
+        -action \"$api_call_name\" \\
+        -appname \"$appid\" \\ " >> runJava.sh
 
 
-# javawrapperversion=$(curl https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/maven-metadata.xml | grep latest |  cut -d '>' -f 2 | cut -d '<' -f 1)
+javawrapperversion=$(curl https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/maven-metadata.xml | grep latest |  cut -d '>' -f 2 | cut -d '<' -f 1)
 
-# #echo "javawrapperversion: $javawrapperversion"
+#echo "javawrapperversion: $javawrapperversion"
 
-# curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
-# chmod 777 runJava.sh
-# cat runJava.sh
-# ./runJava.sh
+curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
+chmod 777 runJava.sh
+cat runJava.sh
+./runJava.sh
