f7d7f9d227babadacac2099600d3902d96ef27a8
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-15 15:29:46 +0530

    report

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,7 +53,5 @@ javawrapperversion=$(curl https://repo1.maven.org/maven2/com/veracode/vosp/api/w
 echo "javawrapperversion: $javawrapperversion"
 
 curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
-chmod 777 runJava.sh
 cat runJava.sh
 ./runJava.sh > output.xml
-
