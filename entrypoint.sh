5fc1641b6a803d216d3fb86a45357e5de786fd74
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-25 19:32:23 +0530

    final changes in shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
 #required parameters
-
 appid=$1
 api_call_name=$2
 vid=$3
@@ -55,59 +54,36 @@ curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vos
 chmod 777 getbuildid.sh
 cat getbuildid.sh
 ./getbuildid.sh > output.xml
- # build info is saved in output.xml
+ # latest apllication buildinfo is saved in output.xml
 
-
-
+#getting Build ID from XML file
 sed -i s+"xmlns=\".*\""+" "+g output.xml
-cat output.xml
+# cat output.xml
 build_id=$(xmllint --xpath 'string(//buildinfo/build/@build_id)' output.xml)
 echo "Build_id: $build_id"
 
+#getting status from XML file
 status=$(xmllint --xpath 'string(//analysis_unit/@status)' output.xml)
 echo "Status: $status"
 
+#getting version from XML file
+version=$(xmllint --xpath 'string(//buildinfo/build/@version)' output.xml)
+echo "Status: $version"
 
+#getting score 
 filepath="summaryreport.xml"
 echo "#!/bin/sh -l" > getscore.sh
 echo ""
 echo "java -jar VeracodeJavaAPI.jar -vid $vid -vkey $vkey -action \"summaryreport\" -buildid $build_id -outputfilepath $filepath" >> getscore.sh
-ls -lrt
 
 chmod 777 getscore.sh
 cat getscore.sh
+#run getscore.sh file
 ./getscore.sh 
-ls -lrt
-# cat summaryreport.xml
 
-# sed -i s+"xmlns=\"https://www.veracode.com/schema/reports/export/1.0\""+" "+g summaryreport.xml
+#Retrieve Score from Summary report
 sed -i s+"xmlns=\".*\""+" "+g summaryreport.xml
 score=$(xmllint --xpath 'string(//summaryreport/static-analysis/@score)' summaryreport.xml)
 echo "Score = $score"
 
-# echo "before"
-# cat output2.xml
-# sed -i s+"</buildinfo>"+" "+g output2.xml
-# echo "after removing"
-# cat output2.xml
-# build_id=$(xmllint --xpath 'string(//build/@build_id)' output2.xml)
 
-
-
-# build_id=$(awk -F 'build_id=' '{print $2}' output.xml | head -c 11) success
-
-# status=$(awk -F 'analysis_type=' '{print $2}' output.xml | tail -c 30)
-
-
-
-# file=output.xml
-
-
-# file="output.xml" 
-  
-# i=1  
-# while read line; do    
-# #Reading each line  
-# echo "Line No. $i : $line"  
-# i=$((i+1))  
-# done < $file  
