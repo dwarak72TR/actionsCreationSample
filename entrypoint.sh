2e74a722658b6eb5b4f0b9fefe213549b48d91c4
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-25 07:35:37 +0530

    added xmllint

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,9 +72,9 @@ done < $file
 
 
 
-res=$(cat output.xml | head -n -4)
+res=$(cat output.xml | tail -n +2)
 echo $res
-myvar=$(xmllint --xpath 'string(//buildinfo/build/@build_id)' output.xml)
+myvar=$(xmllint --xpath 'string(//build/@build_id)' output.xml)
 echo "result="
 echo $myvar
 # build_id=$(awk -F 'build_id=' '{print $2}' output.xml | head -c 11) success
